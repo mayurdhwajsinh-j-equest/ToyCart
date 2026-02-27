4b3a9082701b0c169933272c5c12dfd00d9c2a63
Jadeja Mayurdhwajsinh <mayur.jadeja-i@equestsolutions.net>
2026-02-27 11:26:16 +0530

    updated admin dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,11 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route, Navigate, useLocation } from "react-router-dom";
 import { CartProvider } from "./context/CartContext";
 import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
-// import Collection from "./pages/Collection/Collection";
 import Alltoys from "./pages/Alltoys/Alltoys";
 import Pdp from "./pages/Pdp/Pdp";
-import Footer from "./components/Footer/Footer";
 import Checkout from "./pages/Checkout/Checkout";
 import Order from "./pages/Order/Order";
 import Collection from "./pages/Collection/Collection";
@@ -19,12 +18,17 @@ import AdminProducts from "./pages/Admin/AdminProducts";
 import AdminOrders from "./pages/Admin/AdminOrders";
 import AdminCustomers from "./pages/Admin/AdminCustomers";
 
-function App() {
-  return (
-    <CartProvider>
-      <Navbar />
+function AppContent() {
+  const location = useLocation();
 
-      <main style={{ marginTop: '140px' }}>
+  // Check if current route starts with /admin
+  const isAdminRoute = location.pathname.startsWith("/admin");
+
+  return (
+    <>
+      {!isAdminRoute && <Navbar />}
+
+      <main style={{ marginTop: !isAdminRoute ? "140px" : "0" }}>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Checkout" element={<Checkout />} />
@@ -34,7 +38,9 @@ function App() {
           <Route path="/Collection" element={<Collection />} />
           <Route path="/Product" element={<Product />} />
           <Route path="/Pdp/:id" element={<Pdp />} />
+
           <Route path="/admin/login" element={<AdminLogin />} />
+
           <Route
             path="/admin"
             element={
@@ -50,9 +56,17 @@ function App() {
             <Route path="customers" element={<AdminCustomers />} />
           </Route>
         </Routes>
-
       </main>
-          <Footer />
+
+      {!isAdminRoute && <Footer />}
+    </>
+  );
+}
+
+function App() {
+  return (
+    <CartProvider>
+      <AppContent />
     </CartProvider>
   );
 }
